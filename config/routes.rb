527e5e06e36ba8d2684d1b0c32aca59a5cf631c7
auracode <vijayendra.iitm@gmail.com>
2018-09-08 17:13:48 +0530

    Changed bootstrap files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  get 'users/index'
+
+  get 'users/show'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "pages#index"
 end
